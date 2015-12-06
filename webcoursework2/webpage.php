7e07d23c2673bcb8a06434d5483c8516a82275ba
Stuart <stuartrfraser@icloud.com>
2015-12-06 14:37:05 +0000

    proper id=0 not random

--- webcoursework2/webpage.php
+++ webcoursework2/webpage.php
@@ -3,10 +3,11 @@
 $db = new PDO('mysql:dbname=srf32;host=mysql-server-1;charset=utf8', 'srf32','abcsrf32354') or die("bye");
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$id = rand(1,6);
+$id = 0;
 
-$SQL = $db->prepare("SELECT * FROM twitteraccounts WHERE id=0");
+$SQL = $db->prepare("SELECT * FROM twitteraccounts WHERE id=:id");
 
+$SQL->execute(array(':id'=>$id));
 $result = $SQL->fetch();
 $name = $result['name'];
 
