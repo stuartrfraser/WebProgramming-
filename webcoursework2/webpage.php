fc87d4b6c7bfbe58f42f814b2b07e9ebc0077563
Stuart <stuartrfraser@icloud.com>
2015-12-07 12:20:49 +0000

    Images, description and username now in

--- webcoursework2/webpage.php
+++ webcoursework2/webpage.php
@@ -59,9 +59,7 @@ for($id = 0; $id < 6; $id++) {
 
 
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
 
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -72,6 +70,10 @@ for($id = 0; $id < 6; $id++) {
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Web Progrming</title>
 
     <!-- Bootstrap -->
